2d2d90736546ec33d12b25792dfce05f52a66e3e
Mayra Zanchett Manchein <mayramanchein@yahoo.com.br>
2022-04-04 19:47:27 -0300

    fix image

--- backend/src/main/java/br/edu/unoesc/projeto/heroes/controllers/HeroRESTController.java
+++ backend/src/main/java/br/edu/unoesc/projeto/heroes/controllers/HeroRESTController.java
@@ -5,14 +5,12 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
